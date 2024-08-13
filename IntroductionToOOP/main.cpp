c90eee70c7cb39716e4bc5707ceb780ab85fc044
hawri <hawri@DESKTOP-BMM1386>
2024-08-13 13:21:03 +0300

    Add project files.

---
+++ IntroductionToOOP/main.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+using namespace std;
+
+class Point
+{
+	double x;
+	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this -> x = x;
+	}
+	void set_y(double y)
+	{
+		this -> y = y;
+	}
+	Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(double x)
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "1ArgConstructor:\t" << this << endl;
+	}
+	Point(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+	double distance(Point other)
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
+	void print() const
+	{
+		cout << "X = " << x << "\tY = " << y << endl;
+	}
+};
+
+//#define STRUCT_POINT
+//#define CONSTRUCTORS_CHECK
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef STRUCT_POINT
+	int a;			//объявление переменной 'a' типа 'int'
+	Point A;		//обявление переменной 'A' типа 'Point'
+	//создание объекта 'A' типа 'Point'
+	//создание экземпляра 'A' структуры 'Point'
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+#endif // STRUCT_POINT
+
+#ifdef CONSTRUCTORS_CHECK
+	Point A;
+	//A.set_x(2);
+	//A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+	A.print();
+
+	Point B = 5;
+	B.print();
+
+	Point C(2, 3);
+	C.print();
+
+	A.distance(B);
+#endif // CONSTRUCTORS_CHECK
+
+	Point A(2, 3);
+	Point B(7, 8);
+
+	A.print();
+	B.print();
+
+	cout << "Расстояние от точки 'A' до точки 'B': " << A.distance(B) << endl;
+}
